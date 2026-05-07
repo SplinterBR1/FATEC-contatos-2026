1fb8e12deab47b1257e6e84da51a3c1c527b79a3
Bruno Henrique <bruno.asor96@gmail.com>
2026-05-07 16:00:13 -0300

    atualização do codigo JS, adicionado funcionalidade ao form e o botao de editar funcional no card do contato para utilizar a função de UPDATE

--- app.js
+++ app.js
@@ -9,14 +9,16 @@ const lista = document.getElementById("lista-contatos");
 const form = document.getElementById("form-contato");
 const erroDiv = document.getElementById("erro");
 
+const inputId = document.getElementById("id");
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone");
 const inputFoto = document.getElementById("foto");
 
+
 // Funções
 
-    // Carregar os contatos ao iniciar
+      // Carregar os contatos ao iniciar
 async function carregarContatos() {
   try {
     const contatos = await getContatos();
@@ -26,7 +28,7 @@ async function carregarContatos() {
   }
 }
 
-    // Gerar a lista
+      // Gerar a lista
 function gerarLista(contatos) {
   lista.textContent = "";
 
@@ -34,15 +36,66 @@ function gerarLista(contatos) {
     const card = document.createElement("div");
     card.classList.add("card");
 
-    card.innerHTML = `
-      <img src="${contato.foto}" alt="foto">
-      <h3>${contato.nome}</h3>
-      <p>${contato.email}</p>
-      <p>${contato.telefone}</p>
-    `;
+    const foto = document.createElement("img");
+    const nome = document.createElement("h3");
+    const email = document.createElement("p");
+    const telefone = document.createElement("p");
 
+    const botao = document.createElement("p")
+    botao.innerHTML = `<button onclick="editar('${contato.id}', '${contato.nome}', '${contato.email}', '${contato.telefone}', '${contato.foto}')">Editar</button>`
+
+    foto.src = contato.foto
+    foto.alt = "Foto redonda do contato cadastrado"
+    nome.textContent = contato.nome
+    email.textContent = contato.email
+    telefone.textContent = contato.telefone
+ 
+    card.append(foto, nome, email, telefone, botao)
     lista.appendChild(card);
+    
   });
 }
+
+      // Criar ou atualizar contato 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const contato = {
+    nome: inputNome.value,
+    email: inputEmail.value,
+    telefone: inputTelefone.value,
+    foto: inputFoto.value
+  };
+
+  try {
+    if (inputId.value) {
+      await atualizarContato(inputId.value, contato);
+    } else {
+      await criarContato(contato);
+    }
+
+    form.reset();
+    inputId.value = "";
+    carregarContatos();
+
+  } catch (erro) {
+    mostrarErro(erro.message);
+  }
+});
+
+window.editar = (id, nome, email, telefone, foto) => {
+  inputId.value = id;
+  inputNome.value = nome;
+  inputEmail.value = email;
+  inputTelefone.value = telefone;
+  inputFoto.value = foto;
+};
+
+      // ERRO
+function mostrarErro(mensagem) {
+  erroDiv.textContent = mensagem;
+  setTimeout(() => erroDiv.textContent = "", 3000);
+}
+
 
 carregarContatos();
